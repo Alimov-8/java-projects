3e7ccaba05232f72fdd23bee3ae265dd85a0e04c
Abdullokh Alimov <66109159+Alimov-8@users.noreply.github.com>
2020-10-16 15:30:00 +0500

    Version 1.1
    Menu and other new features added

--- BMICalculator.java
+++ BMICalculator.java
@@ -1,42 +1,132 @@
 /*
+Lab Assignment - 2
 ID:      U1910060
 Name:    Alimov Abdullokh
 Section: 002
 */
 import java.util.Scanner;
-public class BMICalculator {
-    public static void main(String[] args) {
+public class BMIu1910060
+{
+    public static void main(String[] args) 
+	{
         Scanner input = new Scanner(System.in); //Object for Scanner
         double height, weight;
-        int BMI;
-        
-        System.out.print("Height (sm): ");
-        height = (input.nextDouble())/100; // getting height from user and changing it to meters
-        
-        System.out.print("Weight (kg): ");
-        weight = input.nextDouble(); // getting weight from user
+		boolean option = true;
+		int menu; 
+		float BMI;
+		int repitation;
+         
+		while(option) // Loop for Menu
+		{
+			System.out.println(" -- Menu -- ");
+		    System.out.println("1. Calculate in (kg and meters)");
+            System.out.println("2. Calculate in (ib and inches)");
+			System.out.print("Enter Choice: ");
+			menu = (input.nextInt()); // menu choice to use switch cases
+			switch(menu)
+			{
+				case 1: // Case for calculating BMI in (kg and m) 
+				{	
+					System.out.print("Height (meters): ");
+					height = (input.nextDouble()); // getting height from user 
+					while (height <= 0){ // Validation for height
+						System.out.print("Height (meters): ");
+						height = (input.nextDouble()); // getting height again from user
+					}
+					
+					System.out.print("Weight (kg): ");
+					weight = input.nextDouble(); // getting weight from user
+					while (weight <= 0){ // Validation for weight
+						System.out.print("Weight (kg): ");
+						weight = input.nextDouble(); // getting weight again from user
+ 					}
+					
+					BMI = (float) (weight/(height*height)); // calculatin BMI and making it 
+					
+					System.out.println("----------------------");
 
-        BMI = (int) (weight/(height*height)); // calculatin BMI and making it (int)
+					/* Conditions to print BMI with its description */
+					if (BMI < 18.5){
+						System.out.printf("BMI is %.2f Underweight", BMI);
+					}
+					else if (BMI >= 18.5 && BMI<=24.9){
+						System.out.printf("BMI is %.2f Normal ", BMI);
+					}
+					else if (BMI >= 25 && BMI<=29.9){
+						System.out.printf("BMI is %.2f Overweight", BMI);
+					}
+					else if (BMI >= 30 && BMI<=34.9){
+						System.out.printf("BMI is %.2f Obese", BMI);
+					}
+					else {
+						System.out.printf("BMI is %.2f Extremely Obese", BMI);
+					}        
+					
+					// Condition for calculation repitation
+					System.out.println("");
+					System.out.println("----------------------");
+					System.out.println("Enter 1 for Calculating again");
+					System.out.println("Enter any number for Exit");
+					repitation = input.nextInt();
+					if (repitation != 1){
+						option = false;
+					}	
+				}
+					break;
+					
+				case 2:  // Case for calculating BMI in (ib and in) 
+				{
+					System.out.print("Height (inches): ");
+					height = (input.nextDouble()); // getting height from user 
+					// Validation for height
+					while (height <= 0){
+						System.out.print("Height (inches): ");
+						height = (input.nextDouble()); // getting height again from user 
+					}
+					System.out.print("Weight (ib): ");
+					weight = input.nextDouble(); // getting weight from user
+					// Validation for weight
+					while (weight <= 0){
+						System.out.print("Weight (ib): ");
+						weight = input.nextDouble(); // getting weight again from user
+ 					}
+					
+					BMI = (float) ((weight/(height*height))*703); // calculatin BMI and making it 
 
-        /* Conditions to print BMI with its description */
-        if (BMI < 11){
-            System.out.println("BMI is "+BMI+" Extremely Underweight");
-        }
-        else if (BMI >= 12 && BMI<=18){
-            System.out.println("BMI is "+BMI+" Underweight");
-        }
-        else if (BMI >= 19 && BMI<=24){
-            System.out.println("BMI is "+BMI+" Healthy");
-        }
-        else if (BMI >= 25 && BMI<=29){
-            System.out.println("BMI is "+BMI+" Overweight");
-        }
-        else if (BMI >= 30 && BMI<=39){
-            System.out.println("BMI is "+BMI+" Obese");
-        }
-        else {
-            System.out.println("BMI is "+BMI+" Extremely Obese");
-        }
-        
+					System.out.println("----------------------");
+					
+					/* Conditions to print BMI with its description */
+					if (BMI < 18.5){
+						System.out.printf("BMI is %.2f Underweight", BMI);
+					}
+					else if (BMI >= 18.5 && BMI<=24.9){
+						System.out.printf("BMI is %.2f Normal ", BMI);
+					}
+					else if (BMI >= 25 && BMI<=29.9){
+						System.out.printf("BMI is %.2f Overweight", BMI);
+					}
+					else if (BMI >= 30 && BMI<=34.9){
+						System.out.printf("BMI is %.2f Obese", BMI);
+					}
+					else {
+						System.out.printf("BMI is %.2f Extremely Obese", BMI);
+					}        
+					
+					// Condition for calculation repitation
+					System.out.println("");
+					System.out.println("----------------------");
+					System.out.println("Enter 1 for Calculating again");
+					System.out.println("Enter any number for Exit");
+					repitation = input.nextInt();
+					if (repitation != 1){
+						option = false;
+					}
+				}
+					break;
+				default:
+					System.out.println("Menu does not contains that number, Please enter again");
+			}
+		} 
     }
 }
+
